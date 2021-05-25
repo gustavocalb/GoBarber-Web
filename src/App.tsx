5da8ba302d911643eeec0aaeedd9444a50548ad6
FireShark688 <gugacalb@gmail.com>
2021-05-25 17:48:23 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
